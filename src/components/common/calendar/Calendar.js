c6a266336d8fffced0dcee6ced2ee3e680de55ec
Freddie Hoy <freddiehoy@hotmail.com>
2020-05-01 11:54:33 +0100

    Get all the dates of this month to show

--- src/components/common/calendar/Calendar.js
+++ src/components/common/calendar/Calendar.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import WeekDays from './WeekDays';
 import Dates from './Dates';
 import MonthYear from './MonthYear';
-import { getYear, getMonth } from './Helper';
+import { getYear, getMonth, getDatesArray } from './Helper';
 
 const Container = styled.div`
   border: 1px solid black;
@@ -15,50 +15,7 @@ const Month = getMonth();
 
 const Year = getYear();
 
-const DatesArray = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-  '17',
-  '18',
-  '19',
-  '20',
-  '21',
-  '22',
-  '23',
-  '24',
-  '25',
-  '26',
-  '27',
-  '28',
-  '29',
-  '30',
-  '31',
-  '32',
-  '33',
-  '34',
-  '35',
-  '36',
-  '37',
-  '38',
-  '39',
-  '40',
-  '41'
-];
+const DatesArray = getDatesArray();
 
 const Calendar = () => (
   <Container>
--- src/components/common/calendar/Helper.js
+++ src/components/common/calendar/Helper.js
@@ -12,10 +12,59 @@ const Months = [
   'November',
   'December'
 ];
+
 export const getYear = () => +new Date().getFullYear();
 
 export const getMonth = () => {
   const numberMonth = +new Date().getMonth();
 
   return Months[numberMonth];
+};
+
+export const getDatesArray = () => {
+  const todaysDate = new Date();
+  const firstDayOfMonth = new Date(
+    todaysDate.getFullYear(),
+    todaysDate.getMonth(),
+    1
+  );
+  const firstDayOfMonthWeekDayNumber = firstDayOfMonth.getDay() - 1;
+  const numberofDaysInMonth = GetNumberOfDaysInMonth(firstDayOfMonth);
+
+  const monthArray = CreateCountingArray(numberofDaysInMonth);
+
+  const emptyArray = CreateEmpty42Array();
+  emptyArray.splice(
+    firstDayOfMonthWeekDayNumber,
+    numberofDaysInMonth,
+    ...monthArray
+  );
+  const NewArray = emptyArray;
+
+  return NewArray;
+};
+
+const GetNumberOfDaysInMonth = (anyDateInMonth) => {
+  return new Date(
+    anyDateInMonth.getFullYear(),
+    anyDateInMonth.getMonth() + 1,
+    0
+  ).getDate();
+};
+
+const CreateCountingArray = (number) => {
+  let a = [];
+  for (let i = 1; i <= number; i++) {
+    const numberString = i.toString();
+    a.push(numberString);
+  }
+  return a;
+};
+
+const CreateEmpty42Array = () => {
+  let a = [];
+  for (let i = 0; i < 42; i++) {
+    a.push('');
+  }
+  return a;
 };
