ad0bcb03dca2646026d41085df419bd0f96fd95c
Freddie Hoy <freddiehoy@hotmail.com>
2020-05-02 11:04:41 +0100

    Add state for next and previous month

--- src/components/common/calendar/Calendar.js
+++ src/components/common/calendar/Calendar.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import DatePicker from './DatePicker';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import WeekDays from './WeekDays';
+import Dates from './Dates';
+import MonthYear from './MonthYear';
+import { getYear, getMonth, getDatesArray, getHighlightedDay } from './Helper';
 
-const todaysDate = new Date();
+const Container = styled.div`
+  border: 1px solid black;
+  width: 352px;
+  text-align: center;
+`;
 
-// const tomorrow = new Date(todaysDate);
-// tomorrow.setDate(tomorrow.getDate() + 1);
+class Calendar extends Component {
+  state = {
+    targetDate: new Date(),
+    month: '',
+    year: '',
+    today: '',
+    datesArray: []
+  };
 
-// let nextMonth = new Date(todaysDate.setMonth(todaysDate.getMonth() + 1));
+  addMonth = () => {
+    const nextMonth = new Date(
+      this.state.targetDate.setMonth(this.state.targetDate.getMonth() + 1)
+    );
+    this.setState({
+      targetDate: nextMonth,
+      year: getYear(this.state.targetDate),
+      month: getMonth(this.state.targetDate),
+      datesArray: getDatesArray(this.state.targetDate),
+      today: getHighlightedDay(this.state.targetDate)
+    });
+  };
 
-const Calendar = () => (
-  <div>
-    <DatePicker date={todaysDate} />
-  </div>
-);
+  minusMonth = () => {
+    const nextMonth = new Date(
+      this.state.targetDate.setMonth(this.state.targetDate.getMonth() - 1)
+    );
+    this.setState({
+      targetDate: nextMonth,
+      year: getYear(this.state.targetDate),
+      month: getMonth(this.state.targetDate),
+      datesArray: getDatesArray(this.state.targetDate),
+      today: getHighlightedDay(this.state.targetDate)
+    });
+  };
+  // Change month??
+
+  componentDidMount() {
+    this.setState({
+      year: getYear(this.state.targetDate),
+      month: getMonth(this.state.targetDate),
+      datesArray: getDatesArray(this.state.targetDate),
+      today: getHighlightedDay(this.state.targetDate)
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <MonthYear
+          month={this.state.month}
+          year={this.state.year}
+          addMonth={this.addMonth}
+          minusMonth={this.minusMonth}
+        />
+        <WeekDays />
+        <Dates array={this.state.datesArray} today={this.state.today} />
+      </Container>
+    );
+  }
+}
 
 export default Calendar;
