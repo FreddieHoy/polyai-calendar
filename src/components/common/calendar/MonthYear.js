2c8a14ec93820de79cdeed98657efe620536d7bd
Freddie Hoy <freddiehoy@hotmail.com>
2020-05-02 11:08:56 +0100

    Add onClick arrows for next a previous months

--- src/components/common/calendar/MonthYear.js
+++ src/components/common/calendar/MonthYear.js
@@ -4,11 +4,22 @@ import { CalText } from 'config/FontStyles';
 
 const Container = styled.div`
   border: solid 1px black;
+  display: flex;
+  justify-content: space-between;
 `;
 
-const MonthYear = ({ year, month }) => (
+const Arrows = styled.div`
+  margin: 0 10px 3px 10px;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const MonthYear = ({ year, month, addMonth, minusMonth }) => (
   <Container>
+    <Arrows onClick={minusMonth}>{'<'}</Arrows>
     <CalText>{`${month} ${year}`}</CalText>
+    <Arrows onClick={addMonth}>{'>'}</Arrows>
   </Container>
 );
 
