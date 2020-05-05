c40ddab9771febdba3849edda9c6418ab2a25c99
Freddie Hoy <freddiehoy@hotmail.com>
2020-05-05 12:35:35 +0100

    Redo classical Calendar tests removing dive()

--- src/components/common/calendar/__tests__/CalendarOld.js
+++ src/components/common/calendar/__tests__/CalendarOld.js
@@ -4,12 +4,21 @@ import React from 'react';
 import CalendarOld from '../CalendarOld';
 
 describe('CalendarOld', () => {
+  it('renders the correct components', () => {
+    const wrapper = shallow(<CalendarOld />);
+    wrapper.setState({ today: null });
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('adds one month to target date when switchMonth is called with e.target.id as plus', () => {
     const wrapper = shallow(<CalendarOld />);
+    const e = {
+      target: {
+        id: 'plus'
+      }
+    };
     wrapper.setState({ targetDate: new Date(2020, 0, 1) });
-
-    const plusArrow = wrapper.find('MonthYear').dive().find('#plus');
-    plusArrow.simulate('click', { target: { id: 'plus' } });
+    wrapper.instance().switchMonth(e);
 
     const newTargetDate = wrapper.state('targetDate');
     expect(newTargetDate).toEqual(new Date(2020, 1, 1));
@@ -17,10 +26,13 @@ describe('CalendarOld', () => {
 
   it('minus one month to target date when switchMonth is called with e.target.id as plus', () => {
     const wrapper = shallow(<CalendarOld />);
+    const e = {
+      target: {
+        id: ''
+      }
+    };
     wrapper.setState({ targetDate: new Date(2020, 2, 1) });
-
-    const plusArrow = wrapper.find('MonthYear').dive().find('#plus');
-    plusArrow.simulate('click', { target: { id: '' } });
+    wrapper.instance().switchMonth(e);
 
     const newTargetDate = wrapper.state('targetDate');
     expect(newTargetDate).toEqual(new Date(2020, 1, 1));
